56208ff040c663bc6b0113ecfcd6b0f697bf5fd0
Jotaro Aso <jotaro.aso@gmail.com>
2026-03-05 10:45:06 +0900

    同じく

--- js/question.js
+++ js/question.js
@@ -8,21 +8,18 @@ export const typingQuestions = [
     // },
     {
         text: '憲法二二条一項は、日本国内における居住・移転の自由を保障する旨を規定するにとどまり、外国人がわが国に入国することについてはなんら規定していないものである。',
-        romaji: 'kennpounijuunijouikkouha,nihonnkokunainiokerukyojuu/itennnojiyuuwohoshousurumunewokiteisurunitodomari,gaikokujinngawagakunininyuukokusurukotonituitehanannrakiteisiteinaimonodearu.',
         kana:'けんぽうにじゅうにじょういっこうは、にほんこくないにおけるきょじゅう・いてんのじゆうをほしょうするむねをきていするにとどまり、がいこくじんがわがくにににゅうこくすることについてはなんらきていしていないものである。', 
         field: '憲法',
         source: 'マクリーン事件',
     },
     {
         text: '国際慣習法上、国家は外国人を受け入れる義務を負うものではなく、特別の条約がない限り、外国人を自国内に受け入れるかどうか、また、これを受け入れる場合にいかなる条件を付するかを、当該国家が自由に決定することができるものとされている。',
-        romaji: 'kokusaikannshuuhoujou,kokkahagaikokujinnwoukeirerugimuwooumonodehanaku,tokubetunojouyakuganaikagiri,gaikokujinnwojikokunainiukeirerukadouka,mata,korewoukeirerubaainiikanarujoukennwohusurukawo,tougaikokkagajiyuuniketteisurukotogadekirumonotosareteiru.',
         kana: 'こくさいかんしゅうほうじょう、こっかはがいこくじんをうけいれるぎむをおうものではなく、とくべつのじょうやくがないかぎり、がいこくじんをじこくないにうけいれるかどうか、また、これをうけいれるばあいにいかなるじょうけんをふするかを、とうがいこっかがじゆうにけっていすることができるものとされている。',
         field: '憲法',
         source: 'マクリーン事件',
     },
     {
         text: '憲法第三章の諸規定による基本的人権の保障は、権利の性質上日本国民のみをその対象としていると解されるものを除き、わが国に在留する外国人に対しても等しく及ぶものと解すべきである。',
-        romaji: 'kennpoudaisannshounoshokiteiniyorukihonntekijinnkennnohoshouha,kennrinoseisitujounihonkokuminnnomiwosonotaishoutositeirutokaisarerumonowonozoki,wagakuninizairyuusurugaikokujinnnitaisitemohitosikuoyobumonotokaisubekidearu.',
         kana: 'けんぽうだいさんしょうのしょきていによるきほんてきじんけんのほしょうは、けんりのせいしつじょうにほんこくみんのみをそのたいしょうとしているとかいされるものをのぞき、わがくににざいりゅうするがいこくじんにたいしてもひとしくおよぶものとかいすべきである。',
         field: '憲法',
         source: 'マクリーン事件',
